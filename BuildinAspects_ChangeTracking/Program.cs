cd0fef96d608e081cb0ed8f85d1eb8cf7dc36b17
Frank Pommerening <frank@pommerening-online.de>
2015-06-23 18:54:31 +0200

    Add Sample BuildinAspects ChangeTracking

---
+++ BuildinAspects_ChangeTracking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BuildinAspects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vm = new TestViewModel();
+            ((INotifyPropertyChanged) vm).PropertyChanged += (sender, eventArgs) => Console.WriteLine(eventArgs.PropertyName);
+
+            vm.Property1 = "Hallo";
+            vm.Property2 = "Welt";
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BuildinAspects_ChangeTracking/TestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PostSharp.Patterns.Model;
+
+namespace BuildinAspects
+{
+    [NotifyPropertyChanged]
+    public class TestViewModel
+    {
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
+        public string Property3 { get; set; }
+    }
+}
