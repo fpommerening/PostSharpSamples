a1c4b64bd9daf6806ee50583aa6aab8082327c33
Frank Pommerening <frank@pommerening-online.de>
2015-06-23 18:58:15 +0200

    Add Sample BuildinAspects Logging

---
+++ BuildinAspects_Logging/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuildinAspects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestProcessor p = new TestProcessor();
+            p.Method1("Hallo Welt", 42);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BuildinAspects_Logging/TestProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Extensibility;
+
+namespace BuildinAspects
+{
+    [Log(AttributeTargetMemberAttributes = MulticastAttributes.Internal | MulticastAttributes.Public)]
+    public class TestProcessor
+    {
+        public void Method1(string param1, Int32 param2)
+        {
+            MethodA(param1);
+            MethodB(param2);
+        }
+
+        internal void MethodA(string param)
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        internal void MethodB(Int32 param)
+        {
+            System.Threading.Thread.Sleep(2000);
+        }
+
+    }
+}
