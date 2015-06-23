68f0ec9d7d4230caec845491c46d4ac1c37431f4
Frank Pommerening <frank@pommerening-online.de>
2015-06-23 20:01:31 +0200

    Add Sample Webservice Persistence

---
+++ WebservicePersistence/Webservice/EnergyServiceImplementation.cs
@@ -0,0 +1,21 @@
+﻿using FP.Spartakiade2015.WebservicePersistence.BL;
+using FP.Spartakiade2015.WebservicePersistence.Contracts;
+
+namespace FP.Spartakiade2015.WebservicePersistence.Webservice
+{
+    public class EnergyServiceImplementation : IBookCapacity_Binding
+    {
+        public BookCapacityResponseType BookCapacity(BookCapacityRequestType request)
+        {
+            var BL = new BusinessLayer();
+            var freeCapacity = BL.CheckAvailableCapacity(request.Customer, request.ValidFrom, request.ValidTo,
+                request.Quantity, request.Unit);
+            return new BookCapacityResponseType
+            {
+                MessageIdentifier = request.MessageIdentifier,
+                Quantity = freeCapacity,
+                Unit = request.Unit
+            };
+        }
+    }
+}
---
+++ WebservicePersistence/Webservice/PersistWebserviceCallAspect.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Serialization;
+using PostSharp.Aspects;
+
+namespace FP.Spartakiade2015.WebservicePersistence.Webservice
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Method|AttributeTargets.Assembly)]
+    
+    public class PersistWebserviceCallAspect : OnMethodBoundaryAspect 
+    {
+
+        public override bool CompileTimeValidate(MethodBase method)
+        {
+            if (method == null || method.ReflectedType == null)
+            {
+                return false;
+            }
+            foreach (var interfaceType in method.ReflectedType.GetInterfaces())
+            {
+                var methodInInterface = interfaceType.GetMethod(method.Name);
+                if (methodInInterface != null && methodInInterface.GetCustomAttributes(typeof (WebMethodAttribute)).Any())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            base.OnEntry(args);
+            var sessionGuid = Guid.NewGuid();
+            args.MethodExecutionTag = sessionGuid;
+            SaveToFile(string.Format("{0}_Request.xml", sessionGuid.ToString("N")), args.Arguments.First());
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            var sessionGuid = (Guid) args.MethodExecutionTag;
+            if (args.Exception != null)
+            {
+                SaveToFile(string.Format("{0}_Response.xml", sessionGuid.ToString("N")), args.Exception);    
+            }
+            else
+            {
+                SaveToFile(string.Format("{0}_Response.xml", sessionGuid.ToString("N")), args.ReturnValue);    
+            }
+            base.OnExit(args);
+        }
+
+        private void SaveToFile(string fileName, object o)
+        {
+            try
+            {
+                string savePath = Path.Combine(System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath,
+                    "ServiceLog", fileName);
+                if (!Directory.Exists(Path.GetDirectoryName(savePath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+                }
+                var exception = o as Exception;
+                var filecontent = exception != null ? exception.ToString() : Serialize(o);
+                File.WriteAllText(savePath, filecontent);
+            }
+            catch
+            {
+            }
+        }
+
+        private string Serialize(object o)
+        {
+            Type valueType = o.GetType();
+            XmlWriterSettings settings = new XmlWriterSettings { NewLineHandling = NewLineHandling.Entitize };
+            StringWriter sWriter = new StringWriter();
+            XmlSerializer ser = new XmlSerializer(valueType);
+            using (XmlWriter writer = XmlWriter.Create(sWriter, settings))
+            {
+                ser.Serialize(writer, o);
+            }
+            return sWriter.ToString();
+        }
+    }
+}
